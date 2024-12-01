f7af73ae6ca0386723d2df0ef629d4e6626b4d53
izarubin <izarubin@ozon.ru>
2024-12-01 19:23:26 +0300

    +

--- server/internal/app/http/middleware/log.go
+++ server/internal/app/http/middleware/log.go
@@ -1,12 +1,9 @@
 package middleware
-
 import (
 	"fmt"
 	"net/http"
 	"net/http/httputil"
 )
-
-
 
 type LogMux struct {
 	h http.Handler
@@ -21,12 +18,12 @@ func NewLogMux(h http.Handler) http.Handler {
 func (m *LogMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	dumpR, err := httputil.DumpRequest(r, true)
+	
 	if err != nil {
-  	fmt.Println("Failed to dump request", err.Error())
+  		fmt.Println("Failed to dump request", err.Error())
 	} else {
 		fmt.Println("Request", string(dumpR))
 	}
 
 	m.h.ServeHTTP(w, r)
-
 }
