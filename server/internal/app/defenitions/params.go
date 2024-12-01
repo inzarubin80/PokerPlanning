6c5e53b937c5caa6319a3df49882a564f43fe4fd
izarubin <izarubin@ozon.ru>
2024-12-01 19:53:34 +0300

    +

--- server/internal/app/defenitions/params.go
+++ server/internal/app/defenitions/params.go
@@ -1,14 +1,11 @@
 package defenitions
 
 const (
-	
 	ParamPokerID  = "poker_id"
 	ParamTaskID  = "task_id"
 	AuthorizationCode  = "authorization_code"
 	UserID  = "user_id"
 	DisplayName = "display_name"
 	DefaultEmail = "default_email"
-
 	SessionAuthenticationName = "authentication"
-
 )
