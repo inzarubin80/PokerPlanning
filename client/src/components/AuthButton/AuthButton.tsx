8110a0624bcfc068389db088099500f9e2d178ea
izarubin <izarubin@ozon.ru>
2025-03-27 00:41:44 +0300

    +

--- client/src/components/AuthButton/AuthButton.tsx
+++ client/src/components/AuthButton/AuthButton.tsx
@@ -20,7 +20,6 @@ const YandexIcon = () => (
 
 const YandexAuthButton: React.FC = () => {
   
-  
   const clientId = process.env.REACT_APP_YANDEX_OAUTH_CLIENT_ID;
   const authUrl = `https://oauth.yandex.ru/authorize?response_type=code&client_id=${clientId}`;
 
