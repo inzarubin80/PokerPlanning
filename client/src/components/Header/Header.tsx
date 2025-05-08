ad389970472cd3db207194ab398a6cda816b83a6
izarubin <izarubin@ozon.ru>
2025-05-08 16:38:54 +0300

    +

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -2,29 +2,37 @@ import React from 'react';
 import { Box, useMediaQuery, useTheme, Typography } from '@mui/material';
 import { Link } from 'react-router-dom'; // Добавляем импорт Link
 import UserCardButton from '../generic/UserCardButton';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../app/store';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState, AppDispatch } from '../../app/store';
+import { getUser } from '../../features/user/userSlice';
+import { useEffect } from 'react';
+
 
 const Header: React.FC = ({ }) => {
     const theme = useTheme();
     const isMobile = useMediaQuery(theme.breakpoints.down('md'));
     const activeUsersID = useSelector((state: RootState) => state.pokerReducer.room.activeUsersID);
+  
+    const dispatch = useDispatch<AppDispatch>();
+
+      useEffect(() => {
+        dispatch(getUser());
+      }, [dispatch]);
 
     return (
         <Box
-            sx={{
-                p: 1,
-                flexShrink: 0,
-                position: 'sticky',
-                top: 0,
-                zIndex: 10,
-                backgroundColor: 'background.paper',
-                borderBottom: '1px solid',
-                borderColor: 'divider',
-                display: 'flex',
-                justifyContent: 'space-between',
-                alignItems: 'center'
-            }}
+        sx={{
+            p: 1,
+            flexShrink: 0, // Важно!
+            backgroundColor: 'background.paper',
+            borderBottom: '1px solid',
+            borderColor: 'divider',
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            width: '100%',
+            boxSizing: 'border-box' // Важно!
+          }}
         >
             <Box display="flex" alignItems="center" gap={1}>
                 <Link 
